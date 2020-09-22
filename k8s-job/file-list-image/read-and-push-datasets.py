d5d3173b72572110bdc1ddd18caf0a978abec1b7
JenkinsX <jenkinsx@ebi>
2020-09-22 17:48:05 +0100

    Adding file-list image

---
+++ k8s-job/file-list-image/read-and-push-datasets.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import csv
+import json
+import pika
+from pprint import pprint
+
+
+csv.field_size_limit(sys.maxsize)
+
+def read_csv(filename):
+  """Read DATA from CSV in filename"""
+  with open(filename) as f:
+    reader = csv.DictReader(f)
+    DATA = [r for r in reader]
+    return DATA
+
+def write_csv(filename, DATA, header=None):
+  """Write DATA as CSV in filename"""
+  with open(filename, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=header)
+    writer.writeheader()
+    writer.writerows(DATA)
+
+def push_rabbitmq_jobs(data, channel, rabbitmq_queue):
+  for d in data:
+    print(d['dataset'])
+    channel.basic_publish(
+    exchange='',
+    routing_key=rabbitmq_queue,
+    body=json.loads(d),
+    properties=pika.BasicProperties(
+        delivery_mode=2,  # make message persistent
+    ))
+    print(" [x] Sent %r" % d)
+    
+
+
+def main():
+  csv_input = os.environ.get('CSV_INPUT')
+  rabbitmq_url = os.environ.get('BROKER_URL')
+  rabbitmq_queue = os.environ.get('QUEUE')
+
+  data = read_csv(csv_input)
+
+  connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_url))
+  channel = connection.channel()
+  push_rabbitmq_jobs(data,channel,rabbitmq_queue)
+  connection.close()
+
+if __name__ == "__main__":
+  main()
