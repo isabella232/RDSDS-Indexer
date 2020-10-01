828405f46ba80f02ecc83f041ab497028111a291
JenkinsX <jenkinsx@ebi>
2020-10-01 12:10:07 +0100

    Adding pull files

--- k8s-job/file-indexing-image/extract-files.py
+++ k8s-job/file-indexing-image/extract-files.py
@@ -3,41 +3,33 @@ import os
 import csv
 import json
 import pika
+import time
 from pprint import pprint
 
-
+count = 0
 csv.field_size_limit(sys.maxsize)
 
 
-def read_csv(filename):
-    """Read DATA from CSV in filename"""
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        DATA = [r for r in reader]
-        return DATA
-
-
-def write_csv(filename, DATA, header=None):
-    """Write DATA as CSV in filename"""
-    with open(filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(DATA)
-
-
 def pull_rabbitmq_job(ch, method, properties, body):
-    print(" [x] %r" % body)
+   # print(" [x] %r" % body)
+     #time.sleep(2.4)
+     global count 
+     count = count + 1
+     print(count)
     
 
 def main():
     rabbitmq_url = os.environ.get('BROKER_URL')
     rabbitmq_queue = os.environ.get('QUEUE')
 
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=rabbitmq_url))
+    connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_url))
     channel = connection.channel()
     channel.basic_consume(queue=rabbitmq_queue,
                           on_message_callback=pull_rabbitmq_job, auto_ack=True)
+    global count 
+    print(count )
+    channel.start_consuming()
+
     connection.close()
 
 
